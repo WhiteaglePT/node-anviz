3b26b9e7e9d20ee17840e4ea6d93d41165430796
Diogo Raminhos <contacto@diogoraminhos.com>
2020-08-31 19:06:18 +0100

    Add eraseNewRecordsFlag to allow new records flag to be reset

---
+++ examples/eraseNewRecordsFlag.js
@@ -0,0 +1,10 @@
+const Request = require("../lib/request.js"),
+	  fs = require("fs");
+
+let request = new Request("10.0.0.6"); // replace with your device ip
+
+request.execute("eraseNewRecordsFlag", 1).on("error", function(err) {
+	console.info("ERROR", err);
+}).on("complete", function(res, raw){
+	console.info(res);
+});
